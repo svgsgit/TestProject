475d19ef3a69c7e3e4d34c4ab2a33b7e3fb1a415
SVGS Studio <svgsgit@yandex.ru>
2017-10-30 23:57:29 +0700

    Initital commit

---
+++ engine/engine.cs
@@ -0,0 +1,19 @@
+using System;
+
+class TestClass {
+	float _time = 0;
+
+	public float time {
+		get {
+			return _time;
+		}
+
+		set {
+			_time = value;
+		}
+	}
+
+	void NewFn() {
+		
+	}
+}
---
+++ test.cs
@@ -0,0 +1,15 @@
+using System;
+
+class TestClass {
+	float _time = 0;
+
+	public float time {
+		get {
+			return _time;
+		}
+
+		set {
+			_time = value;
+		}
+	}
+}
